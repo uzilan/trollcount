081b5d63a0318d2b9fc273a29551adc10e23d492
Uzi Landsmann <uzi.landsmann@gmail.com>
2012-04-10 13:48:32 +0200

    Added documentation

--- src/main/scala/se/diversify/trollcount/ComplexTrollNumber.scala
+++ src/main/scala/se/diversify/trollcount/ComplexTrollNumber.scala
@@ -1,12 +1,18 @@
 package se.diversify.trollcount
 
-class ComplexTrollNumber(trollNumbers: List[TrollNumber]) extends TrollNumber(trollNumbers.foldLeft(0)(_ + _)) {
-
+/**Represents a complex [[se.diversify.trollcount.TrollNumber]], such as Many-Many-One
+ * @param trollNumbers a List of [[se.diversify.trollcount.TrollNumber]]s which builds this complex [[se.diversify.trollcount.TrollNumber]]
+ */
+class ComplexTrollNumber private[trollcount](trollNumbers: List[TrollNumber]) extends TrollNumber(trollNumbers.foldLeft(0)(_ + _)) {
   override def toString: String = trollNumbers.mkString("-")
 }
 
+/**Allows converting an [[scala.Int]] into a [[se.diversify.trollcount.ComplexTrollNumber]]*/
 object ComplexTrollNumber {
-
+  /**Allows converting an [[scala.Int]] into a [[se.diversify.trollcount.ComplexTrollNumber]]
+   * @param i the [[scala.Int]] to convert
+   * @return the converted [[se.diversify.trollcount.ComplexTrollNumber]]
+   */
   implicit def int2ComplexTrollNumbers(i: Int): TrollNumber = {
     val manys = i / 4
     val rest = i - manys * 4
--- src/main/scala/se/diversify/trollcount/TrollNumber.scala
+++ src/main/scala/se/diversify/trollcount/TrollNumber.scala
@@ -1,9 +1,17 @@
 package se.diversify.trollcount
 
+/*Exception that should be thrown if the user uses TrollNumbers that are bigger then what a troll can grasp */
 class TrollsDontDoBigNumbersException extends Exception
 
+/**This class allows creating troll numbers, as well as adding them to each other. For example:
+ * {{{
+ * val two = Two
+ * val five = Many-One
+ * val fourteen = five + Many-Many-One
+ * }}}
+ * @param value the numeric value of this [[se.diversify.trollcount.TrollNumber]]
+ */
 class TrollNumber private[trollcount](val value: Int) {
-
   override def toString: String = value match {
     case 1 => "One"
     case 2 => "Two"
@@ -12,12 +20,31 @@ class TrollNumber private[trollcount](val value: Int) {
     case 16 => "Lots"
   }
 
+  /**Allows joining [[se.diversify.trollcount.TrollNumber]]s together, for example:
+   * {{{
+   * val five = Many-One
+   * val eleven = Many-Many-Three
+   * }}}
+   * @param other the other [[se.diversify.trollcount.TrollNumber]] to join with this one
+   * @return a new [[se.diversify.trollcount.TrollNumber]] with this one combined with the other
+   */
   def -(other: TrollNumber): TrollNumber = this + other
 
+  /**Allows adding one [[se.diversify.trollcount.TrollNumber]] to another, for example:
+   * {{{
+   * val three = One + Two
+   * val twelve = Many-Many-One + Three
+   * }}}
+   * @param other the other [[se.diversify.trollcount.TrollNumber]] to add to this one
+   * @return a new [[se.diversify.trollcount.TrollNumber]] with this one added to the other
+   */
   def +(other: TrollNumber): TrollNumber = TrollNumber(this.value + other.value)
 
 }
 
+/**Allows creating simple and complex troll numbers, as well as converting [[scala.Int]]s to
+ * [[se.diversify.trollcount.TrollNumber]]s and vise verse
+ */
 object TrollNumber {
   val One = new TrollNumber(1)
   val Two = new TrollNumber(2)
@@ -27,8 +54,16 @@ object TrollNumber {
 
   private[trollcount] def apply(i: Int): TrollNumber = int2TrollNumber(i)
 
+  /**Allows converting a [[se.diversify.trollcount.TrollNumber]] into an [[scala.Int]]
+   * @param tn the [[se.diversify.trollcount.TrollNumber]] to convert
+   * @return the [[se.diversify.trollcount.TrollNumber]] value as an [[scala.Int]]
+   */
   implicit def TrollNumber2Int(tn: TrollNumber): Int = tn.value
 
+  /**Allows converting an [[scala.Int]] into a [[se.diversify.trollcount.TrollNumber]]
+   * @param i the [[scala.Int]] to convert
+   * @return the [[scala.Int]]'s value as a [[se.diversify.trollcount.TrollNumber]]
+   */
   implicit def int2TrollNumber(i: Int): TrollNumber = {
     if (i > 16) throw new TrollsDontDoBigNumbersException
 
